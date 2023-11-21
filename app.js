3e052b2341ff3d429bd0f6126796e73796a8bd0b
Shashank Patel <patel.shash@northeastern.edu>
2023-11-20 22:56:52 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import "dotenv/config";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import cors from "cors";
+import CourseRoutes from './courses/routes.js';
+import ModuleRoutes from './modules/routes.js';
+import AssignmentRoutes from './assignments/routes.js';
+const app = express();
+app.use(cors());
+app.use(express.json());
+AssignmentRoutes(app);
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+Hello(app);
+app.listen(process.env.PORT || 4000);
