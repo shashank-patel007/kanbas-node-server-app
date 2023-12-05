a66e937eee6874335a459590bafabe98671bcb3d
Shashank Patel <49752716+shashank-patel007@users.noreply.github.com>
2023-12-05 00:17:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL
+    origin: "https://keen-babka-4a4617.netlify.app"
 }));
 const sessionOptions = {
     secret: "any string",
